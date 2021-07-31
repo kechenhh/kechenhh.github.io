00fa541e0c64221d40f255591ccc01bc66909e51
kechenhh <923407526@qq.com>
2021-07-31 17:16:50 +0800

    '网盘'

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -72,6 +72,7 @@ module.exports = [
       { text: '学习', link: '/pages/f2a556/' },
       { text: '面试', link: '/pages/aea6571b7a8bae86/' },
       { text: '友情链接', link: '/friends/' },
+	  { text: '网盘', link: 'http://107.173.176.177:5244/home' },
     ],
   },
   { text: '关于', link: '/about/' },

905f0d8cec89355d9bc8795b6444d56592c825ad
kechenhh <923407526@qq.com>
2022-08-22 22:46:40 +0800

    模板更新

--- theme-vdoing/node_utils/getSidebarData.js
+++ theme-vdoing/node_utils/getSidebarData.js
@@ -11,7 +11,7 @@ let catalogueData = {}; // 目录页数据
  * @param {String} sourceDir .md文件所在源目录(一般是docs目录)
  * @param {Boolean} collapsable  是否可折叠
  */
-function createSidebarData (sourceDir, collapsable) {
+function createSidebarData(sourceDir, collapsable) {
   const sidebarData = {};
   const tocs = readTocs(sourceDir);
   tocs.forEach(toc => { // toc是每个目录的绝对路径
@@ -43,7 +43,7 @@ module.exports = createSidebarData;
  * 读取指定目录下的文件绝对路径
  * @param {String} root 指定的目录
 */
-function readTocs (root) {
+function readTocs(root) {
   const result = [];
   const files = fs.readdirSync(root); // 读取目录,返回数组，成员是root底下所有的目录名 (包含文件夹和文件)
   files.forEach(name => {
@@ -60,7 +60,7 @@ function readTocs (root) {
  * 将碎片化文章目录(_posts)映射为对应的侧边栏配置数据
  * @param {String} root
  */
-function mapTocToPostSidebar (root) {
+function mapTocToPostSidebar(root) {
   let postSidebar = [] // 碎片化文章数据
   const files = fs.readdirSync(root); // 读取目录（文件和文件夹）,返回数组
 
@@ -85,12 +85,16 @@ function mapTocToPostSidebar (root) {
     }
 
     const contentStr = fs.readFileSync(file, 'utf8') // 读取md文件内容，返回字符串
-    const { data } = matter(contentStr) // 解析出front matter数据
-    const permalink = data.permalink || ''
+    const { data } = matter(contentStr, {}) // 解析出front matter数据
+    const { permalink = '', titleTag = '' } = data || {}
     if (data.title) {
       title = data.title
     }
-    postSidebar.push([filename, title, permalink]);  // [<路径>, <标题>, <永久链接>]
+    const item = [filename, title, permalink]
+    if (titleTag) {
+      item.push(titleTag)
+    }
+    postSidebar.push(item);  // [<路径>, <标题>, <永久链接>, <?标题标签>]
   })
 
   return postSidebar
@@ -104,7 +108,7 @@ function mapTocToPostSidebar (root) {
  * @param {String} prefix
  */
 
-function mapTocToSidebar (root, collapsable, prefix = '') {
+function mapTocToSidebar(root, collapsable, prefix = '') {
   let sidebar = []; // 结构化文章侧边栏数据
   const files = fs.readdirSync(root); // 读取目录（文件和文件夹）,返回数组
 
@@ -114,7 +118,26 @@ function mapTocToSidebar (root, collapsable, prefix = '') {
     if (filename === '.DS_Store') { // 过滤.DS_Store文件
       return
     }
-    let [order, title, type] = filename.split('.');
+    // let [order, title, type] = filename.split('.');
+
+    const fileNameArr = filename.split('.')
+    const isDir = stat.isDirectory()
+    let order = '', title = '', type = '';
+    if (fileNameArr.length === 2) {
+      order = fileNameArr[0];
+      title = fileNameArr[1];
+    } else {
+      const firstDotIndex = filename.indexOf('.');
+      const lastDotIndex = filename.lastIndexOf('.');
+      order = filename.substring(0, firstDotIndex);
+      type = filename.substring(lastDotIndex + 1);
+      if (isDir) {
+        title = filename.substring(firstDotIndex + 1);
+      } else {
+        title = filename.substring(firstDotIndex + 1, lastDotIndex);
+      }
+    }
+
     order = parseInt(order, 10);
     if (isNaN(order) || order < 0) {
       log(chalk.yellow(`warning: 该文件 "${file}" 序号出错，请填写正确的序号`))
@@ -123,7 +146,7 @@ function mapTocToSidebar (root, collapsable, prefix = '') {
     if (sidebar[order]) { // 判断序号是否已经存在
       log(chalk.yellow(`warning: 该文件 "${file}" 的序号在同一级别中重复出现，将会被覆盖`))
     }
-    if (stat.isDirectory()) { // 是文件夹目录
+    if (isDir) { // 是文件夹目录
       sidebar[order] = {
         title,
         collapsable, // 是否可折叠，默认true
@@ -135,8 +158,8 @@ function mapTocToSidebar (root, collapsable, prefix = '') {
         return;
       }
       const contentStr = fs.readFileSync(file, 'utf8') // 读取md文件内容，返回字符串
-      const { data } = matter(contentStr) // 解析出front matter数据
-      const permalink = data.permalink || ''
+      const { data } = matter(contentStr, {}) // 解析出front matter数据
+      const { permalink = '', titleTag = '' } = data || {}
 
       // 目录页对应的永久链接，用于给面包屑提供链接
       const { pageComponent } = data
@@ -147,7 +170,9 @@ function mapTocToSidebar (root, collapsable, prefix = '') {
       if (data.title) {
         title = data.title
       }
-      sidebar[order] = [prefix + filename, title, permalink];  // [<路径>, <标题>, <永久链接>]
+      const item = [prefix + filename, title, permalink]
+      if (titleTag) item.push(titleTag)
+      sidebar[order] = item;  // [<路径>, <标题>, <永久链接>, <?标题标签>]
 
     }
   })
--- theme-vdoing/node_utils/setFrontmatter.js
+++ theme-vdoing/node_utils/setFrontmatter.js
@@ -7,26 +7,28 @@ const readFileList = require('./modules/readFileList');
 const { type, repairDate, dateFormat } = require('./modules/fn');
 const log = console.log
 const path = require('path');
+const os = require('os');
 
 const PREFIX = '/pages/'
-
 
 /**
  * 给.md文件设置frontmatter(标题、日期、永久链接等数据)
  */
-function setFrontmatter (sourceDir, themeConfig) {
-
-  const isCategory = themeConfig.category
-  const isTag = themeConfig.tag
-  const categoryText = themeConfig.categoryText || '随笔'
-
-  const files = readFileList(sourceDir); // 读取所有md文件数据
+function setFrontmatter(sourceDir, themeConfig) {
+  const { category: isCategory, tag: isTag, categoryText = '随笔', extendFrontmatter } = themeConfig
+  const files = readFileList(sourceDir) // 读取所有md文件数据
+  // 扩展自定义生成frontmatter
+  const extendFrontmatterStr = extendFrontmatter ?
+    jsonToYaml.stringify(extendFrontmatter)
+      .replace(/\n\s{2}/g, "\n")
+      .replace(/"|---\n/g, "")
+    : '';
 
   files.forEach(file => {
     let dataStr = fs.readFileSync(file.filePath, 'utf8');// 读取每个md文件内容
 
     // fileMatterObj => {content:'剔除frontmatter后的文件内容字符串', data:{<frontmatter对象>}, ...}
-    const fileMatterObj = matter(dataStr);
+    const fileMatterObj = matter(dataStr, {});
 
     if (Object.keys(fileMatterObj.data).length === 0) { // 未定义FrontMatter数据
       const stat = fs.statSync(file.filePath);
@@ -40,19 +42,15 @@ function setFrontmatter (sourceDir, themeConfig) {
 
       let cateLabelStr = '';
       categories.forEach(item => {
-        cateLabelStr += '\r\n  - ' + item
+        cateLabelStr += os.EOL + '  - ' + item
       });
 
       let cateStr = '';
       if (!(isCategory === false)) {
-        cateStr = '\r\ncategories:' + cateLabelStr
+        cateStr = os.EOL + 'categories:' + cateLabelStr
       };
 
-        // 注意下面这些反引号字符串的格式会映射到文件
-        //       const cateStr = isCategory === false ? '' : `
-        // categories:
-        //   - ${categories[0]}${categories[1] ? '\r\n  - ' + categories[1] : ''}`;
-
+      // 注意下面这些反引号字符串的格式会映射到文件
       const tagsStr = isTag === false ? '' : `
 tags:
   - `;
@@ -60,55 +58,64 @@ tags:
       const fmData = `---
 title: ${file.name}
 date: ${dateStr}
-permalink: ${getPermalink()}${file.filePath.indexOf('_posts') > -1 ? '\r\nsidebar: auto' : ''}${cateStr}${tagsStr}
----`;
+permalink: ${getPermalink()}${file.filePath.indexOf('_posts') > -1 ? os.EOL + 'sidebar: auto' : ''}${cateStr}${tagsStr}
+${extendFrontmatterStr}---`;
 
-      fs.writeFileSync(file.filePath, `${fmData}\r\n${fileMatterObj.content}`); // 写入
+      fs.writeFileSync(file.filePath, `${fmData}${os.EOL}${fileMatterObj.content}`); // 写入
       log(chalk.blue('tip ') + chalk.green(`write frontmatter(写入frontmatter)：${file.filePath} `))
 
     } else { // 已有FrontMatter
-      const matterData = fileMatterObj.data;
-      let mark = false;
+      let matterData = fileMatterObj.data;
+      let hasChange = false;
 
       // 已有FrontMatter，但是没有title、date、permalink、categories、tags数据的
       if (!matterData.hasOwnProperty('title')) { // 标题
         matterData.title = file.name;
-        mark = true;
+        hasChange = true;
       }
 
       if (!matterData.hasOwnProperty('date')) { // 日期
         const stat = fs.statSync(file.filePath);
         matterData.date = dateFormat(getBirthtime(stat));
-        mark = true;
+        hasChange = true;
       }
 
       if (!matterData.hasOwnProperty('permalink')) { // 永久链接
         matterData.permalink = getPermalink();
-        mark = true;
+        hasChange = true;
       }
 
       if (file.filePath.indexOf('_posts') > -1 && !matterData.hasOwnProperty('sidebar')) { // auto侧边栏，_posts文件夹特有
         matterData.sidebar = "auto";
-        mark = true;
+        hasChange = true;
       }
 
       if (!matterData.hasOwnProperty('pageComponent') && matterData.article !== false) { // 是文章页才添加分类和标签
         if (isCategory !== false && !matterData.hasOwnProperty('categories')) { // 分类
           matterData.categories = getCategories(file, categoryText)
-          mark = true;
+          hasChange = true;
         }
-
         if (isTag !== false && !matterData.hasOwnProperty('tags')) { // 标签
           matterData.tags = [''];
-          mark = true;
+          hasChange = true;
         }
       }
 
-      if (mark) {
+      // 扩展自动生成frontmatter的字段
+      if (type(extendFrontmatter) === 'object') {
+        Object.keys(extendFrontmatter).forEach(keyName => {
+          if (!matterData.hasOwnProperty(keyName)) {
+            matterData[keyName] = extendFrontmatter[keyName]
+            hasChange = true;
+          }
+        })
+      }
+
+      if (hasChange) {
         if (matterData.date && type(matterData.date) === 'date') {
           matterData.date = repairDate(matterData.date) // 修复时间格式
         }
-        const newData = jsonToYaml.stringify(matterData).replace(/\n\s{2}/g, "\n").replace(/"/g, "") + '---\r\n' + fileMatterObj.content;
+        const newData = jsonToYaml.stringify(matterData).replace(/\n\s{2}/g, "\n").replace(/"/g, "") + '---' + os.EOL + fileMatterObj.content;
         fs.writeFileSync(file.filePath, newData); // 写入
         log(chalk.blue('tip ') + chalk.green(`write frontmatter(写入frontmatter)：${file.filePath} `))
       }
@@ -118,7 +125,7 @@ permalink: ${getPermalink()}${file.filePath.indexOf('_posts') > -1 ? '\r\nsideba
 }
 
 // 获取分类数据
-function getCategories (file, categoryText) {
+function getCategories(file, categoryText) {
   let categories = []
 
   if (file.filePath.indexOf('_posts') === -1) {
@@ -129,23 +136,35 @@ function getCategories (file, categoryText) {
     let ind = filePathArr.indexOf('docs')
     if (ind !== -1) {
       while (filePathArr[++ind] !== undefined) {
-        categories.push(filePathArr[ind].split('.').pop()) // 获取分类
+        const item = filePathArr[ind]
+        const firstDotIndex = item.indexOf('.');
+        categories.push(item.substring(firstDotIndex + 1) || '') // 获取分类
+        // categories.push(filePathArr[ind].split('.').pop()) // 获取分类
       }
     }
   } else {
-    categories.push(categoryText)
+    // 碎片化文章的分类生成
+    const matchResult = file.filePath.match(/_posts\/(\S*)\//);
+    const resultStr = matchResult ? matchResult[1] : ''
+    const resultArr = resultStr.split('/').filter(Boolean)
+
+    if (resultArr.length) {
+      categories.push(...resultArr)
+    } else {
+      categories.push(categoryText)
+    }
   }
   return categories
 }
 
 // 获取文件创建时间
-function getBirthtime (stat) {
+function getBirthtime(stat) {
   // 在一些系统下无法获取birthtime属性的正确时间，使用atime代替
   return stat.birthtime.getFullYear() != 1970 ? stat.birthtime : stat.atime
 }
 
 // 定义永久链接数据
-function getPermalink () {
+function getPermalink() {
   return `${PREFIX + (Math.random() + Math.random()).toString(16).slice(2, 8)}/`
 }
 
